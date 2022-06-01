8fa3135cdd4b75ee38d0b1b3a42fc90088e7af64
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-01 16:39:13 +0500

    REACT | REDUX project

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,2 +1,12 @@
+import { LOGIN } from "../redux/types/authTypes"
+
 export const API_PATH = "https://testing-api-dostavka.herokuapp.com/user"
 export const TOKEN = "delivery-app-token"
+
+export const config = {
+    headers: {
+        "Accept": 'application/json',
+        "Authorization": "Token " + localStorage.getItem(LOGIN),
+        "Content-Type": "multipart/form-data"
+    }
+}
